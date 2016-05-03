59301934cdf2fc12ac4042c19751b0f81dac63ed
jasmo2 <jasmo2.com@gmail.com>
2016-05-03 00:04:15 -0500

    Build Json before passing to segue

--- NewsBlog/Controllers/PostController.swift
+++ NewsBlog/Controllers/PostController.swift
@@ -7,30 +7,53 @@
 //
 
 import UIKit
+import SwiftyJSON
+
 
 class PostController: UIViewController {
     
-    var passedValue: String = ""
+    
+    @IBOutlet weak var newsView: UIWebView!
+    var selectedPost:JSON!
+    
+//    required init?(coder aDecoder: NSCoder) {
+//        print("init \(selectedPost)")
+//        
+//        super.init(coder: aDecoder)
+//    }
+    
+    
+    deinit {
+        print("deinit PostController")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("init \(selectedPost)")
+        print("selectedPost[\"url\"] \(selectedPost["url"])")
 
-        // Do any additional setup after loading the view.
+        let url = NSURL ( string: selectedPost["url"].string!);
+        let requestObj = NSURLRequest(URL: url!);
+        
+        newsView.loadRequest(requestObj)
+        self.dismissViewControllerAnimated(true, completion: {});        // Do any additional setup after loading the view.
     }
 
+    /*
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
+    */
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //         Get the new view controller using segue.destinationViewController.
+    //         Pass the selected object to the new view controller.
+        
     }
-    */
+    
 
 }
